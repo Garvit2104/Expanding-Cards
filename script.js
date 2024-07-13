b30ba9b3ed50faa36235fef8bbb2be65214ce91e
Garvit <garvitrangi21@gmail.com>
2024-07-13 12:15:58 +0530

    "Card Explorer"

---
+++ script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel');
+
+function removeAllActiveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active');
+    });
+}
+
+panels.forEach(panel =>{
+    panel.addEventListener('click', () =>{
+        removeAllActiveClasses();
+        panel.classList.add('active');
+
+    });
+});
+
+
